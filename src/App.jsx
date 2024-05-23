52ebffbc99b2a98e2b07294728a987bf658b6a2d
kwakdohye <131122840+kwakdohye@users.noreply.github.com>
2024-05-23 10:28:00 +0900

    App.jsx 주석추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Wrapper } from "@googlemaps/react-wrapper";
 import Navi from "./components/Navi";
 import List from "./components/List";
 
+// 수정
 // Wrapper 컴포넌트는 맵 초기화 할 수 있음
 // wrapper은 내부가 아니라 외부에 있어야 하므로 부모 컴포넌트인 app에 있어야 한다.
 import "./index.css";
